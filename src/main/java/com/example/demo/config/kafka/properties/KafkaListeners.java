18abd41b6a5b43903ae8606d83d25289d2dc30bf
Vlad Bazin <Vlad Bazin>
2023-06-18 21:18:40 +0300

    changes for kafka

--- src/main/java/com/example/demo/config/kafka/properties/KafkaListeners.java
+++ src/main/java/com/example/demo/config/kafka/properties/KafkaListeners.java
@@ -1,7 +1,6 @@
 package com.example.demo.config.kafka.properties;
 
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,11 +8,16 @@ public class KafkaListeners {
 
     @KafkaListener(topics = "my-topic1" , groupId = "group-id")
     void listaner(String data){
-            System.out.println("i am listaner and i catch your message =)))   look :" + data);
+            System.out.println("i am listaner 1 and i catch your message look :" + data);
     }
 
     @KafkaListener(topics = "my-topic2" , groupId = "group-id")
     void listener1(String data){
-            System.out.println("i am listaner1 and i catch your message =)))   look :" + data);
+            System.out.println("i am listaner 2 and i catch your message look :" + data);
+    }
+
+    @KafkaListener(topics = "my-topic-transaction" , groupId = "group-id")
+    void listenerForTransaction(String data){
+        System.out.println("i am listaner for transaction and i catch your message look :" + data);
     }
 }
